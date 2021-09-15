dc4dbffcadcf40b8024295875124a11dd264038a
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-09-15 15:28:03 +0600

    Add patch , put , get method to work with db.Add serializer and decorator that serialize the result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort,fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -29,35 +30,66 @@ video_put_args.add_argument("name",type=str,help="Name of the video",required=Tr
 video_put_args.add_argument("views",type=int,help="Views of the video",required=True)
 video_put_args.add_argument("likes",type=int,help="Likes on the video",required=True)
 
-videos = {}
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name",type=str,help="Name of the video")
+video_update_args.add_argument("views",type=int,help="Views of the video")
+video_update_args.add_argument("likes",type=int,help="Likes on the video")
 
 
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404,message="Can not find video...") #404	Not Found
-
-def abort_if_video__exist(video_id):
-    if video_id in videos:
-        abort(409,message="Video alreary exist with this id...") # 409	Conflict
+# how an object should be serialize
+resource_fields = {
+    'id':fields.String,
+    'name':fields.String,
+    'views':fields.Integer,
+    'likes':fields.Integer
+}
 
 class Video(Resource):
 
+    @marshal_with(resource_fields)
     def get(self,video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404,message="Could not find video with that id")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self,video_id):
-        abort_if_video__exist(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201 # status return that it is created
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409,message='Video id taken...')
+        video = VideoModel(id=video_id,name=args['name'],views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201 # status return that it is created
     
+    #update
+    @marshal_with(resource_fields)
+    def patch(self,video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404,message="Video can not update, can not update")
+        if args["name"]:
+            result.name = args['name']
+        if args["views"]:
+            result.views = args['views']
+        if args["likes"]:
+            result.likes = args['likes']
+        
+        # do not need to add any changes , just update and commit it
+        db.session.commit()
+
+        return result
+
+
     def delete(self,video_id):
         abort_if_video_id_doesnt_exist(video_id)
         del videos[video_id]
         return '', 204 # 204  No Content
 
-api.add_resource(Video,"/helloworld/<int:video_id>")
+api.add_resource(Video,"/video/<int:video_id>")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,18 +2,6 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-data = [{"likes":10,"name":"How to make ball","views":1600},
-        {"likes":1000,"name":"Write REST API","views":8000},
-        {"likes":8000,"name":"Replace you ego","views":16000}
-        ]
-
-for i in range(len(data)):
-    response = requests.put(BASE + 'helloworld/'+str(i), data[i])
-    print(response.json())
-
 input()
-response = requests.delete(BASE + 'helloworld/0')
-print(response)# as we return not json serializable obj we do not need put .json(), it will return error
-input()
-response = requests.get(BASE + 'helloworld/2')
+response = requests.patch(BASE + 'video/2',{"views":99,"likes":101})
 print(response.json())
